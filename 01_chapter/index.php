a1aa3b60bf68a367f3c0be2c25cf5d27f52dfe47
nkobaidze <nika.kobaidze@gmail.com>
2022-06-29 14:42:51 +0400

    [DONE] ch 1

---
+++ 01_chapter/index.php
@@ -0,0 +1,23 @@
+<?php 
+
+# Server-side programming scripting
+
+# Command-line scripting
+
+# Running Linux, Windows, MacOS 
+
+# Supports wide-rangeing databases
+// - MySQL
+// - PostgreSQL
+// - Oracle
+// - Sybase
+// - MSSQL
+// - DB2 and ODBC-compliant databases
+// - NoSQL databases: CouchDB, MongoDB
+
+# History
+// Creator: Rasmus Lerdorf 1994
+
+# Configuration file
+// php.ini
+// phpinfo()
