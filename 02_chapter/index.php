52a3157c5cbcbbb2a85243408feee3287e352e09
nkobaidze <nika.kobaidze@gmail.com>
2022-07-04 16:04:32 +0400

    [ADD] flow control staitment

--- 02_chapter/index.php
+++ 02_chapter/index.php
@@ -141,16 +141,36 @@ $c = $b;
 
 // print_r(get_defined_vars());
 
-function print_mem()
-{
-   /* Currently used memory */
-   $mem_usage = memory_get_usage();
+// function print_mem()
+// {
+//    /* Currently used memory */
+//    $mem_usage = memory_get_usage();
    
-   /* Peak memory usage */
-   $mem_peak = memory_get_peak_usage();
+//    /* Peak memory usage */
+//    $mem_peak = memory_get_peak_usage();
 
-   echo 'The script is now using: <strong>' . round($mem_usage / 1024) . 'KB</strong> of memory.<br>';
-   echo 'Peak usage: <strong>' . round($mem_peak / 1024) . 'KB</strong> of memory.<br><br>';
+//    echo 'The script is now using: <strong>' . round($mem_usage / 1024) . 'KB</strong> of memory.<br>';
+//    echo 'Peak usage: <strong>' . round($mem_peak / 1024) . 'KB</strong> of memory.<br><br>';
+// }
+
+// print_mem();
+
+
+// DECLARE
+
+declare(ticks=1);
+
+function tick_handler(){
+    echo "Tick handler -> \n";
 }
 
-print_mem();
+
+
+register_tick_function('tick_handler');
+
+
+$a = 1; 
+$a = 2; 
+$a = 3; 
+$a = 4; 
+$a = 5; 
